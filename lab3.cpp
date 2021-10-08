ba9708056db894818b4b3d8363a4b2c75b36488d
keyow <kirill500123@gmail.com>
2021-10-08 22:49:48 +0300

    Divided one loop into two (it makes program work faster)

--- lab3.cpp
+++ lab3.cpp
@@ -17,11 +17,15 @@ int main()
  
 	cout << "\nRandomly generated numbers: \n";
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) 
+	{
 		arr.push_back(rand() % 2000 - 1000);
 		if (abs(arr[i] % 2) == 1)
 			res += 1;
+	}
 
+	for (int i = 0; i < n; i++)
+	{
 		cout << setw(4) << arr[i] << "\t";
 		if (i % 4 == 3) cout << endl;
 	}
